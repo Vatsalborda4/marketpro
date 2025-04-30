2923664d0313f9fe7d6f3b52e1e730a0d442a0d0
Abhi112004 <abhigajera046@gmail.com>
2025-04-30 15:27:24 +0530

    Add FlashSales component for displaying daily flash sales with countdown timers

---
+++ src/components/FlashSales.jsx
@@ -0,0 +1,105 @@
+import React, { memo, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const FlashSales = () => {
+  return (
+    <div className="flash-sales">
+      <div className="container ">
+        <div className="section-heading">
+          <div className="section-heading-inner">
+            <h5 className="section-heading-inner-item-left">
+              Flash Sales Today
+            </h5>
+            <div className="section-heading-inner-item-right">
+              <Link
+                to="/shop"
+                className="section-heading-link"
+              >
+                View All Deals
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="flash-sales-slider">
+          <div className="flash-sales-slider-item">
+            <img
+              src="assets/images/flash-sale-bg1.png"
+              alt=""
+              className=" flash-sales-item-bg"
+            />
+            <div className="flash-sales-slider-item-thumb">
+              <img src="/assets/images/flash-sale-img1.png" alt="" />
+            </div>
+            <div className="flash-sales-slider-item-content">
+              <h6 className="flash-sales-slider-item-content-title">
+                Fresh Vegetables
+              </h6>
+                <ul className="flash-sales-slider-countdown-list">
+                  <li className="flash-sales-slider-countdown-list-inner">
+                    <span className="days" />1 Days
+                  </li>
+                  <li className="flash-sales-slider-countdown-list-inner">
+                    <span className="hours" />2 Hours
+                  </li>
+                  <li className="flash-sales-slider-countdown-list-inner">
+                    <span className="minutes" />
+                    30 Min
+                  </li>
+                  <li className="flash-sales-slider-countdown-list-inner">
+                    <span className="seconds" />
+                    39 Sec
+                  </li>
+                </ul>
+              <Link to="/shop" className="flash-sales-slider-btn">
+                Shop Now
+                <span className="flash-sales-slider-btn-inner">
+                  <i className="ph ph-arrow-right" />
+                </span>
+              </Link>
+            </div>
+          </div>
+
+          <div className="flash-sales-slider-item">
+            <img
+              src="assets/images/flash-sale-bg2.png"
+              alt=""
+              className=" flash-sales-item-bg"
+            />
+            <div className="flash-sales-slider-item-thumb">
+              <img src="assets/images/flash-sale-img2.png" alt="" />
+            </div>
+            <div className="flash-sales-slider-item-content">
+              <h6 className="flash-sales-slider-item-content-title">
+                Daily Snacks
+              </h6>
+                <ul className="flash-sales-slider-countdown-list">
+                  <li className="flash-sales-slider-countdown-list-inner">
+                    <span className="days" />1 Days
+                  </li>
+                  <li className="flash-sales-slider-countdown-list-inner">
+                    <span className="hours" />2 Hours
+                  </li>
+                  <li className="flash-sales-slider-countdown-list-inner">
+                    <span className="minutes" />
+                    30 Min
+                  </li>
+                  <li className="flash-sales-slider-countdown-list-inner">
+                    <span className="seconds" />
+                    39 Sec
+                  </li>
+                </ul>
+              <Link to="/shop" className="flash-sales-slider-btn">
+                Shop Now
+                <span className="flash-sales-slider-btn-inner">
+                  <i className="ph ph-arrow-right" />
+                </span>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FlashSales;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Banner from "../components/Banner";
 import Feature from "../components/Feature";
 import Promotional from "../components/Promotional";
+import FlashSales from "../components/FlashSales";
 
 const HomePage = () => {
   return (
@@ -11,7 +12,9 @@ const HomePage = () => {
 
       <Feature />
 
-      <Promotional/>
+      <Promotional />
+
+      <FlashSales />
     </>
   );
 };
