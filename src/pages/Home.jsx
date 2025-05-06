837a9b994fee01682a308a8036f1e5be928431e3
Abhi112004 <abhigajera046@gmail.com>
2025-05-06 18:30:15 +0530

    Add Newsletter component with subscription form and background images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,6 +14,7 @@ import TopVendors from "../components/TopVendors";
 import Delivery from "../components/Delivery";
 import Brands from "../components/Brands";
 import Shipping from "../components/Shipping";
+import Newsletter from "../components/Newsletter";
 
 const Home = () => {
   return (
@@ -48,6 +49,7 @@ const Home = () => {
       <NewArrival />
       
       <Shipping/>
+      <Newsletter/>
     </>
   );
 };
